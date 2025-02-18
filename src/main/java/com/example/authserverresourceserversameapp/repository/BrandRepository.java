d3d4376140f8f56f481235a936b14c314bf7af30
Anton Momot <anton30momot@gmail.com>
2025-02-18 11:32:24 +0200

    optimized selecting types and brands during adding and updating products

--- src/main/java/com/example/authserverresourceserversameapp/repository/BrandRepository.java
+++ src/main/java/com/example/authserverresourceserversameapp/repository/BrandRepository.java
@@ -1,12 +1,15 @@
 package com.example.authserverresourceserversameapp.repository;
 
 import com.example.authserverresourceserversameapp.model.Brand;
+import com.example.authserverresourceserversameapp.model.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
     Brand getOneByName(String name);
+
+    Brand getOneByTypesType(Type type);
 
     List<Brand> getAllByNameNotLikeOrderByName(String name);
 
--- src/main/java/com/example/authserverresourceserversameapp/service/ProductServiceImpl.java
+++ src/main/java/com/example/authserverresourceserversameapp/service/ProductServiceImpl.java
@@ -2,7 +2,10 @@ package com.example.authserverresourceserversameapp.service;
 
 import com.example.authserverresourceserversameapp.dto.*;
 import com.example.authserverresourceserversameapp.exception.*;
-import com.example.authserverresourceserversameapp.model.*;
+import com.example.authserverresourceserversameapp.model.Brand;
+import com.example.authserverresourceserversameapp.model.Photo;
+import com.example.authserverresourceserversameapp.model.Product;
+import com.example.authserverresourceserversameapp.model.Type;
 import com.example.authserverresourceserversameapp.repository.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -129,14 +132,11 @@ public class ProductServiceImpl implements ProductService {
         }
         Type type = typeRepository.findById(dto.getTypeId()).get();
         Brand brand = brandRepository.findById(dto.getBrandId()).get();
-        TypeBrand typeBrand = typeBrandRepository.findFirstByTypeAndBrand(type, brand);
         if (dto.getId() == 0) {
             product = new Product();
             type.addProduct(product);
             brand.addProduct(product);
-            if (typeBrand == null) {
-                type.addBrand(brand);
-            }
+            addTypeBrand(type, brand);
         } else if (dto.getId() > 0) {
             product = productRepository.findById(dto.getId()).get();
             Type productType = product.getType();
@@ -145,25 +145,30 @@ public class ProductServiceImpl implements ProductService {
                 productBrand.removeProduct(product);
                 brand.addProduct(product);
                 type.removeBrand(productBrand);
-                type.addBrand(brand);
             } else if (!type.equals(productType) && brand.equals(productBrand)) {
                 productType.removeProduct(product);
                 type.addProduct(product);
                 productType.removeBrand(brand);
-                type.addBrand(brand);
             } else if (!type.equals(productType) && !brand.equals(productBrand)) {
                 productType.removeProduct(product);
                 type.addProduct(product);
                 productBrand.removeProduct(product);
                 brand.addProduct(product);
                 productType.removeBrand(productBrand);
-                type.addBrand(brand);
             }
+            addTypeBrand(type, brand);
         }
         assert product != null;
         product.setName(dto.getName());
         product.setPrice(dto.getPrice());
         return productRepository.save(product).getId();
+    }
+
+    public void addTypeBrand(Type type, Brand brand) {
+        Brand exists = brandRepository.getOneByTypesType(type);
+        if (exists == null) {
+            type.addBrand(brand);
+        }
     }
 
     /**
